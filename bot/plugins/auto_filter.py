bfa25d9bec3495f32492a8f119c013d35190d332
joker153 <77005994+joker153@users.noreply.github.com>
2021-10-10 08:20:18 -0700

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -221,7 +221,9 @@ async def auto_filter(bot, update):
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
-
+            await asyncio.sleep(5)
+            await Send_message.delete()
+            
         except ButtonDataInvalid:
             print(result[0])
         
